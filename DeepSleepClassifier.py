5d6f35943e2fda714c0a1cab1af3e53ddee10488
Francisco Guerrero <afguerrerohernan@wpi.edu>
2017-06-25 07:31:27 -0400

    - Using original xception architecture

--- DeepSleepClassifier.py
+++ DeepSleepClassifier.py
@@ -142,50 +142,49 @@ class DeepSleepClassifier(object):
     def build_model(self):
         optimizer = 'sgd'
         input_shape = Input(shape=(15000, 3, 1))
-        x = Conv2D(32, (3, 1), strides=(2, 1), use_bias=False, name='block1_conv1')(input_shape)
+        x = Conv2D(32, (3, 3), strides=(2, 2), use_bias=False, name='block1_conv1')(input_shape)
         x = BatchNormalization(name='block1_conv1_bn')(x)
         x = Activation('relu', name='block1_conv1_act')(x)
-        x = Conv2D(64, (3, 1), use_bias=False, name='block1_conv2')(x)
+        x = Conv2D(64, (3, 3), use_bias=False, name='block1_conv2')(x)
         x = BatchNormalization(name='block1_conv2_bn')(x)
         x = Activation('relu', name='block1_conv2_act')(x)
 
-        residual = Conv2D(128, (1, 1), strides=(2, 1),
-                          padding='same', use_bias=False)(x)
+        residual = Conv2D(128, (1, 1), strides=(2, 2), padding='same', use_bias=False)(x)
         residual = BatchNormalization()(residual)
 
-        x = SeparableConv2D(128, (3, 1), padding='same', use_bias=False, name='block2_sepconv1')(x)
+        x = SeparableConv2D(128, (3, 3), padding='same', use_bias=False, name='block2_sepconv1')(x)
         x = BatchNormalization(name='block2_sepconv1_bn')(x)
         x = Activation('relu', name='block2_sepconv2_act')(x)
-        x = SeparableConv2D(128, (3, 1), padding='same', use_bias=False, name='block2_sepconv2')(x)
+        x = SeparableConv2D(128, (3, 3), padding='same', use_bias=False, name='block2_sepconv2')(x)
         x = BatchNormalization(name='block2_sepconv2_bn')(x)
 
-        x = MaxPooling2D((3, 1), strides=(2, 1), padding='same', name='block2_pool')(x)
+        x = MaxPooling2D((3, 3), strides=(2, 2), padding='same', name='block2_pool')(x)
         x = layers.add([x, residual])
 
-        residual = Conv2D(256, (1, 1), strides=(2, 1), padding='same', use_bias=False)(x)
+        residual = Conv2D(256, (1, 1), strides=(2, 2), padding='same', use_bias=False)(x)
         residual = BatchNormalization()(residual)
 
         x = Activation('relu', name='block3_sepconv1_act')(x)
-        x = SeparableConv2D(256, (3, 1), padding='same', use_bias=False, name='block3_sepconv1')(x)
+        x = SeparableConv2D(256, (3, 3), padding='same', use_bias=False, name='block3_sepconv1')(x)
         x = BatchNormalization(name='block3_sepconv1_bn')(x)
         x = Activation('relu', name='block3_sepconv2_act')(x)
-        x = SeparableConv2D(256, (3, 1), padding='same', use_bias=False, name='block3_sepconv2')(x)
+        x = SeparableConv2D(256, (3, 3), padding='same', use_bias=False, name='block3_sepconv2')(x)
         x = BatchNormalization(name='block3_sepconv2_bn')(x)
 
-        x = MaxPooling2D((3, 1), strides=(2, 1), padding='same', name='block3_pool')(x)
+        x = MaxPooling2D((3, 3), strides=(2, 2), padding='same', name='block3_pool')(x)
         x = layers.add([x, residual])
 
-        residual = Conv2D(728, (1, 1), strides=(2, 1), padding='same', use_bias=False)(x)
+        residual = Conv2D(728, (1, 1), strides=(2, 2), padding='same', use_bias=False)(x)
         residual = BatchNormalization()(residual)
 
         x = Activation('relu', name='block4_sepconv1_act')(x)
-        x = SeparableConv2D(728, (3, 1), padding='same', use_bias=False, name='block4_sepconv1')(x)
+        x = SeparableConv2D(728, (3, 3), padding='same', use_bias=False, name='block4_sepconv1')(x)
         x = BatchNormalization(name='block4_sepconv1_bn')(x)
         x = Activation('relu', name='block4_sepconv2_act')(x)
-        x = SeparableConv2D(728, (3, 1), padding='same', use_bias=False, name='block4_sepconv2')(x)
+        x = SeparableConv2D(728, (3, 3), padding='same', use_bias=False, name='block4_sepconv2')(x)
         x = BatchNormalization(name='block4_sepconv2_bn')(x)
 
-        x = MaxPooling2D((3, 1), strides=(2, 1), padding='same', name='block4_pool')(x)
+        x = MaxPooling2D((3, 3), strides=(2, 2), padding='same', name='block4_pool')(x)
         x = layers.add([x, residual])
 
         for i in range(8):
@@ -193,36 +192,35 @@ class DeepSleepClassifier(object):
             prefix = 'block' + str(i + 5)
 
             x = Activation('relu', name=prefix + '_sepconv1_act')(x)
-            x = SeparableConv2D(728, (3, 1), padding='same', use_bias=False, name=prefix + '_sepconv1')(x)
+            x = SeparableConv2D(728, (3, 3), padding='same', use_bias=False, name=prefix + '_sepconv1')(x)
             x = BatchNormalization(name=prefix + '_sepconv1_bn')(x)
             x = Activation('relu', name=prefix + '_sepconv2_act')(x)
-            x = SeparableConv2D(728, (3, 1), padding='same', use_bias=False, name=prefix + '_sepconv2')(x)
+            x = SeparableConv2D(728, (3, 3), padding='same', use_bias=False, name=prefix + '_sepconv2')(x)
             x = BatchNormalization(name=prefix + '_sepconv2_bn')(x)
             x = Activation('relu', name=prefix + '_sepconv3_act')(x)
-            x = SeparableConv2D(728, (3, 1), padding='same', use_bias=False, name=prefix + '_sepconv3')(x)
+            x = SeparableConv2D(728, (3, 3), padding='same', use_bias=False, name=prefix + '_sepconv3')(x)
             x = BatchNormalization(name=prefix + '_sepconv3_bn')(x)
 
             x = layers.add([x, residual])
 
-        residual = Conv2D(1024, (1, 1), strides=(2, 1),
-                          padding='same', use_bias=False)(x)
+        residual = Conv2D(1024, (1, 1), strides=(2, 2), padding='same', use_bias=False)(x)
         residual = BatchNormalization()(residual)
 
         x = Activation('relu', name='block13_sepconv1_act')(x)
-        x = SeparableConv2D(728, (3, 1), padding='same', use_bias=False, name='block13_sepconv1')(x)
+        x = SeparableConv2D(728, (3, 3), padding='same', use_bias=False, name='block13_sepconv1')(x)
         x = BatchNormalization(name='block13_sepconv1_bn')(x)
         x = Activation('relu', name='block13_sepconv2_act')(x)
-        x = SeparableConv2D(1024, (3, 1), padding='same', use_bias=False, name='block13_sepconv2')(x)
+        x = SeparableConv2D(1024, (3, 3), padding='same', use_bias=False, name='block13_sepconv2')(x)
         x = BatchNormalization(name='block13_sepconv2_bn')(x)
 
-        x = MaxPooling2D((3, 1), strides=(2, 1), padding='same', name='block13_pool')(x)
+        x = MaxPooling2D((3, 3), strides=(2, 2), padding='same', name='block13_pool')(x)
         x = layers.add([x, residual])
 
-        x = SeparableConv2D(1536, (3, 1), padding='same', use_bias=False, name='block14_sepconv1')(x)
+        x = SeparableConv2D(1536, (3, 3), padding='same', use_bias=False, name='block14_sepconv1')(x)
         x = BatchNormalization(name='block14_sepconv1_bn')(x)
         x = Activation('relu', name='block14_sepconv1_act')(x)
 
-        x = SeparableConv2D(2048, (3, 1), padding='same', use_bias=False, name='block14_sepconv2')(x)
+        x = SeparableConv2D(2048, (3, 3), padding='same', use_bias=False, name='block14_sepconv2')(x)
         x = BatchNormalization(name='block14_sepconv2_bn')(x)
         x = Activation('relu', name='block14_sepconv2_act')(x)
 
